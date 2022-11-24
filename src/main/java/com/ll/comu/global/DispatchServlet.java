6b48071bb82d3ceeb06047ca6fb5242c0ce807b4
Jae-Soon Kwon <91954574+Jae-soon@users.noreply.github.com>
2022-11-24 22:32:58 +0900

    Feat : 수정

--- src/main/java/com/ll/comu/global/DispatchServlet.java
+++ src/main/java/com/ll/comu/global/DispatchServlet.java
@@ -25,11 +25,13 @@ public class DispatchServlet extends HttpServlet {
                     case "/usr/article/write" -> articleController.showWrite(rq);
                     case "/usr/article/detail" -> articleController.showDetail(rq);
                     case "/usr/article/delete" -> articleController.doDelete(rq);
+                    case "/usr/article/modify" -> articleController.showModify(rq);
                 }
                 break;
             case "POST":
                 switch (rq.getActionPath()) {
                     case "/usr/article/write" -> articleController.doWrite(rq);
+                    case "/usr/article/modify" -> articleController.doModify(rq);
                 }
                 break;
         }
