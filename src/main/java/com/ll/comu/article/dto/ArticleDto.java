da986a2d651e28570f17f689f82adc3bd7fd3c81
Jae-Soon Kwon <91954574+Jae-soon@users.noreply.github.com>
2022-11-24 22:37:30 +0900

    Feat : Lombok 사용

--- src/main/java/com/ll/comu/article/dto/ArticleDto.java
+++ src/main/java/com/ll/comu/article/dto/ArticleDto.java
@@ -1,46 +1,13 @@
 package com.ll.comu.article.dto;
 
+import lombok.*;
+
+@ToString
+@AllArgsConstructor
+@Getter
+@Setter
 public class ArticleDto {
     private long id;
     private String title;
     private String content;
-
-    @Override
-    public String toString() {
-        return "ArticleDto{" +
-                "id =" + id +
-                ", title = '" + title + '\'' +
-                ", content = '" + content + '\'' +
-                '}';
-    }
-
-    public ArticleDto(long id, String title, String content) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
 }
